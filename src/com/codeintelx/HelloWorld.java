cf3a7841a2d6062f233d7eba15d5a7ed0ac732d6
Daijona Revell <daidai@Daijonas-MacBook-Air.local>
2020-08-08 11:30:32 -0400

    Uploading Hello World File

---
+++ src/com/codeintelx/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.codeintelx;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        String b = "Hello World! We about to learn Git out here!";
+        System.out.println(b);
+    }
+}
